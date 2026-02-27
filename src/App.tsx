8bb300449eb4302f9f3d2a03b28dd900c062ae91
nimo <nimonit1@gmail.com>
2026-02-28 00:16:16 +0900

    fix:404エラー

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import TaskList from './pages/TaskList'
